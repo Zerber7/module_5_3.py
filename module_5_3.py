bc7228732b6f3fc71f345557d6f9536a10b71d1f
Zerber7 <zerber_@mail.ru>
2024-10-29 19:50:37 +0300

    Add files via upload

---
+++ module_5_3.py
@@ -0,0 +1,124 @@
+'''
+Необходимо дополнить класс House следующими специальными методами:
+__eq__(self, other) - должен возвращать True, если количество этажей одинаковое у self и у other.
+Методы __lt__(<), __le__(<=), __gt__(>), __ge__(>=), __ne__(!=) должны присутствовать в классе и возвращать результаты сравнения по соответствующим операторам. Как и в методе __eq__ в сравнении участвует кол-во этажей.
+__add__(self, value) - увеличивает кол-во этажей на переданное значение value, возвращает сам объект self.
+__radd__(self, value), __iadd__(self, value) - работают так же как и __add__ (возвращают результат его вызова).
+
+Пример выполняемого кода:
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
+
+Название: ЖК Эльбрус, кол-во этажей: 10
+Название: ЖК Акация, кол-во этажей: 20
+False
+Название: ЖК Эльбрус, кол-во этажей: 20
+True
+Название: ЖК Эльбрус, кол-во этажей: 30
+Название: ЖК Акация, кол-во этажей: 30
+False
+True
+False
+True
+False
+'''
+
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        if 0 < new_floor <= self.number_of_floors:
+            for floor in range(1, new_floor + 1):
+                print(floor)
+        else:
+            print("Такого этажа не существует")
+
+    # h1 = House('ЖК Горский', 18)
+    # h2 = House('Домик в деревне', 2)
+
+    # h1.go_to(5)
+    # h2.go_to(10)
+
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название: {self.name}, Количество этажей: {self.number_of_floors}'
+
+
+    def __eq__(self, other):
+        return self.number_of_floors == other. number_of_floors
+
+    def __lt__(self, other):
+        return self.number_of_floors < other. number_of_floors
+
+    def __le__(self, other):
+        return self.number_of_floors <= other. number_of_floors
+
+    def __gt__(self, other):
+        return self.number_of_floors > other. number_of_floors
+
+    def __ge__(self, other):
+        return self.number_of_floors >= other. number_of_floors
+
+    def __ne__(self, other):
+        return self.number_of_floors != other. number_of_floors
+
+    def __add__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, other):
+        return self + other
+
+    def __radd__(self, other):
+        return self + other
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2)
+
+h1 = h1 + 10
+print(h1)
+print(h1 == h2)
+
+h1 += 10
+print(h1)
+
+h2 = 10 + h2
+print(h2)
+
+print(h1 > h2)
+print(h1 >= h2)
+print(h1 < h2)
+print(h1 <= h2)
+print(h1 != h2)
+
